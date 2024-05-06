ac65e1077008a2a71d63d8fa7caa9c80dab9df9d
sidnb13 <sidnbaskaran@gmail.com>
2024-05-06 07:07:57 +0000

    update

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import itertools
+import os
 from typing import Dict
 
 import torch
 import torch.distributed as dist
 import wandb
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from torch import nn
 from torch.utils.data import DataLoader
 from transformers import (
@@ -74,7 +75,7 @@ def train(
     train_itr = itertools.cycle(train_dataloader)
     val_itr = itertools.cycle(validation_dataloader)
     grad_acc_steps = 0
-    examples_counter = 0
+    train_examples_counter = val_examples_counter = 0
 
     for step in range(total_steps):
         train_batch = next(train_itr)
@@ -86,48 +87,101 @@ def train(
 
         (loss / config.train.gradient_accumulation_steps).backward()
         grad_acc_steps += 1
-        examples_counter += len(train_batch[next(iter(train_batch.keys()))])
-
-        batch_metrics = {
-            "loss/train": loss.detach().item()
-            if loss.shape[0] == 1
-            else loss.detach().mean().item(),
-            "lr": opt.param_groups[0]["lr"],
-            "counters/examples": examples_counter,
-            "counters/step": step,
-            "counters/epoch": step / len(train_dataloader),
-        }
+        train_examples_counter += len(train_batch[next(iter(train_batch.keys()))])
 
         if grad_acc_steps == config.train.gradient_accumulation_steps == 0:
             grad_acc_steps = 0
             grad_norm = torch.nn.utils.clip_grad_norm_(
                 editor.parameters(), config.train.max_grad_norm
             )
-            batch_metrics["grad_norm"] = grad_norm.item()
             opt.step()
             opt.zero_grad()
             scheduler.step()
 
+            batch_metrics = {
+                "loss/train": loss.detach().item()
+                if loss.shape[0] == 1
+                else loss.detach().mean().item(),
+                "lr": opt.param_groups[0]["lr"],
+                "counters/train_examples": train_examples_counter,
+                "counters/step": step,
+                "counters/epoch": step / len(train_dataloader),
+                "grad_norm": grad_norm.item(),
+            }
+
+            if rank == 0:
+                logger.info(batch_metrics)
+                if config.wandb.enabled:
+                    wandb.log(batch_metrics)
+
+        if step > 0 and step % config.train.eval_interval == 0:
+            with torch.no_grad():
+                val_batch = next(val_itr)
+                if config.train.loss == "kl":
+                    loss = compute_kl_loss(editor, val_batch, rank, world_size)
+                else:
+                    loss = compute_ce_loss(editor, val_batch, rank, world_size)
+
+            val_examples_counter += len(val_batch[next(iter(val_batch.keys()))])
+
+            batch_metrics = {
+                "counters/val_examples": val_examples_counter,
+                "loss/val": loss.item(),
+            }
+
         if (
-            config.train.log_interval > 0
-            and step % config.train.log_interval == 0
-            and rank == 0
+            rank == 0
+            and (step > 0 and step % config.train.save_interval == 0)
+            and config.train.do_save
         ):
-            logger.info(batch_metrics)
-            if config.wandb.enabled:
-                wandb.log(batch_metrics)
+            pass
+
+
+def save_model_checkpoint(
+    step,
+    model: GPT2Editor,
+    optimizer: torch.optim.Optimizer,
+    scheduler: torch.optim.lr_scheduler._LRScheduler,
+    config: DictConfig,
+):
+    state_dict = {
+        "hypernetwork": model.hypernetwork.state_dict(),
+        "step": step,
+        "opt": optimizer.state_dict(),
+        "scheduler": scheduler.state_dict(),
+    }
+    ckpt_folder = os.path.join(config.ckpt_dir, config.exp_name, "step-{}".format(step))
+    torch.save(state_dict, ckpt_folder)
+    OmegaConf.save(config, os.path.join(ckpt_folder, "config.yaml"))
+
+
+def load_model_checkpoint(
+    ckpt_folder: str,
+    model: GPT2Editor,
+    optimizer: torch.optim.Optimizer,
+    scheduler: torch.optim.lr_scheduler._LRScheduler,
+    config: DictConfig,
+):
+    pass
 
 
 def compute_kl_loss(
-    editor: GPT2Editor,
-    batch: Dict,
-    rank,
-    world_size,
+    editor,
+    batch: Dict[str, torch.Tensor],
+    rank: int,
+    world_size: int,
     average: bool = True,
+    stop_editing_idx: int = None,
 ):
     # run the hypernetwork
     batch = slice_and_move_batch_for_device(batch, rank, world_size)
-    editor_out = editor(**batch)
+    editor_out = editor(
+        editor_input_ids=batch["editor_input_ids"],
+        editor_attention_mask=batch["editor_attention_mask"],
+        target_input_ids=batch["target_input_ids"],
+        target_attention_mask=batch["target_attention_mask"],
+        stop_editing_idx=stop_editing_idx,
+    )
 
     edited_target_logprobs = torch.nn.functional.log_softmax(editor_out.logits, dim=-1)
     target_mask = batch["target_attention_mask"] > 0
@@ -143,7 +197,11 @@ def compute_kl_loss(
         lengths_B = torch.sum(batch["editor_input_ids"] != pad_token, dim=1)
 
         # Create an empty tensor to store the predictions
-        shape = (len(lengths_A), edited_target_logprobs.shape[-2], 50257)
+        shape = (
+            len(lengths_A),
+            edited_target_logprobs.shape[-2],
+            editor.target_model.config.vocab_size,
+        )
         extracted_logits = torch.full(
             shape, torch.nan, device=edited_target_logprobs.device
         )
@@ -169,17 +227,23 @@ def compute_kl_loss(
 
 
 def compute_ce_loss(
-    editor: GPT2Editor,
-    batch: Dict,
-    rank,
-    world_size,
+    editor,
+    batch: Dict[str, torch.Tensor],
+    rank: int,
+    world_size: int,
     stop_editing_idx: int = None,
     average_log_prob: bool = True,
     token_level: bool = False,
 ):
     # run the hypernetwork
     batch = slice_and_move_batch_for_device(batch, rank, world_size)
-    editor_out = editor(**batch, stop_editing_idx=stop_editing_idx)
+    editor_out = editor(
+        editor_input_ids=batch["editor_input_ids"],
+        editor_attention_mask=batch["editor_attention_mask"],
+        target_input_ids=batch["target_input_ids"],
+        target_attention_mask=batch["target_attention_mask"],
+        stop_editing_idx=stop_editing_idx,
+    )
 
     loss_mask = batch["target_attention_mask"] > 0
     labels = torch.where(loss_mask, batch["target_input_ids"], 0)
